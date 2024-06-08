70e003159c01e46fdbd4930be860331d8e09f1b1
Risav Karna <git@risav.dev>
2024-06-08 14:15:58 +0545

    feat(editable): add the folder docs

--- src/reactor/reactor.spec.ts
+++ src/reactor/reactor.spec.ts
@@ -1,36 +1,47 @@
-import { describe, test, expect } from "bun:test";
-import { join, folder } from "./reactor";
+import { describe, expect, test } from "bun:test";
 import type { Monad } from "../patterns";
+import { folder, join } from "./reactor";
 
 describe("folder", () => {
   test("fmap should apply a function to the value", () => {
-    expect(folder(42).fmap(x => x + x).val()).toEqual(84);
+    expect(
+      folder(42)
+        .fmap((x) => x + x)
+        .val()
+    ).toEqual(84);
   });
-  
+
   test("cartesian ap should apply functions to the value as a cartesian cross product", () => {
-    const answer = folder(2).cart([x => x +  x + x, x => x * x]).val();
-    expect(answer).toBeArray();  
+    const answer = folder(2)
+      .cart([(x) => x + x + x, (x) => x * x])
+      .val();
+    expect(answer).toBeArray();
     expect(answer[0]).toEqual(6);
     expect(answer[1]).toEqual(4);
-  })
-  
+  });
+
   test("fold ap (similar to zip ap) should apply functions to the value as a chain of composed functions", () => {
-    expect(folder(2).fold([x => x + x, x => x * x]).val()).toEqual(16);  
-  })  
+    expect(
+      folder(2)
+        .fold([(x) => x + x, (x) => x * x])
+        .val()
+    ).toEqual(16);
+  });
 
   test("pure should create a new folder with the provided value", () => {
-    const pied = folder('life, universe, everything');
+    const pied = folder("life, universe, everything");
     expect(pied.pure(42).val()).toEqual(42);
-  })
+  });
 
   test("bind should bind a function to the value", () => {
-    const answer = folder(42).bind(x => folder(x + x)).val();
+    const answer = folder(42)
+      .bind((x) => folder(x + x))
+      .val();
     expect(answer).toEqual(84);
-  })
+  });
 
   test("join should unbox a monad", () => {
     const answer: Monad<Monad<number>> = folder(folder(42));
     expect(join(answer).val()).toEqual(42);
-   })
-})
-
+  });
+});
--- src/reactor/reactor.ts
+++ src/reactor/reactor.ts
@@ -4,16 +4,40 @@ export type Value<T> = {
   val: () => T
 } & { __brand: 'value' };
 
-export type Pipe = <T, U>(f: (x: T) => U) => Pipe;
-export type Compose = <T, U>(f: (x: T) => U) => Compose;
-export type Node<T> = Value<T> | Pipe | Compose;
+/**
+ * Checks if the given object is a value of type T.
+ *
+ * @param {any} obj - The object to check.
+ * @return {obj is Value<T>} - True if the object is a value of type T, false otherwise.
+ */
+export function isValue<T>(obj: any): obj is Value<T> {
+    return obj?.__brand === 'value' && obj?.val() !== undefined;
+}
 
-export const isValue = <T>(obj: any): obj is Value<T> => obj?.__brand === 'value';
-
+/**
+ * A function that takes a value of type T and returns a value of type U.
+*/
 export type Func<T, U> = (x: T) => U;
 
-export const join: <U>(moMo: Monad<Monad<U>>) => Monad<U> 
-  = (moMo) => moMo.val();
+/**
+ * Unwraps a boxed folder monad and returns the inner monad.
+*/
+export function join<U>(moMo: Monad<Monad<U>>): Monad<U> {
+  return moMo.val();
+}
+
+/**
+ * Creates a folder object with a value and functions to manipulate it.
+ *
+ * @param {T} x - The initial value of the folder.
+ * @return {Monad<T>} - An object with the following functions:
+ *   - fmap: Maps the value of the folder using the provided function.
+ *   - ap: Applies an array of functions to the value of the folder.
+ *   - fold: Applies a reducer function to the value of the folder.
+ *   - pure: Creates a new folder with the provided value.
+ *   - bind :: m t -> (t -> m u) -> m u, where m t is introduced from the val in closure
+ */
+export const pure = <T>(x: T): Monad<T> => folder(x);
 
 /**
  * Creates a folder object with a value and functions to manipulate it.
@@ -32,20 +56,7 @@ export const folder: <T>(val: T) => Monad<T>
     fmap: <U>(f: (x: T) => U) => folder(f(val)),
     cart: <U>(fa: Array<Func<T, U>>) => folder(fa.map(f => f(val))),
     fold: <U extends T>(fa: Array<Func<T, U>>) => folder(fa.reduce((acc, f) => f(acc), val)),
-    pure: (x: T) => folder(x),
+    pure,
     bind: <U>(f: (x: T) => Monad<U>) => join(folder(f(val))),
     join    
 }) as Monad<T>;
-
-export const zipper: <T, U>(x: T, y: U) => Monad<[T, U]>
-  = <T, U>(x: T, y: U) => folder([x, y]);
-
-
-export type LinkedList<T> = {
-  head: T,
-  tail?: LinkedList<T>
-}
-
-export function composeFuncList<T extends Func<T, any>>(initial: T, ll: LinkedList<T>): any {
-  return ll.tail ? composeFuncList(ll.head(initial), ll.tail) : ll.head(initial);
-}
